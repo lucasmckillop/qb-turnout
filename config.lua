375d06ab3cffd603e1095dfb6551c3b20ed7a809
peachy#9999 <90997593+lucasmckillop@users.noreply.github.com>
2022-04-12 22:48:15 +1000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+config = config or {}
+
+config.turnout = {
+    ["arms"]        = { item = 165,  texture = 8},  -- Arms
+    ["tshirt"]     = { item = 68, texture = 0},  -- T-Shirt
+    ["torso2"]      = { item = 80, texture = 0},  -- Jackets
+    ["vest"]        = { item = 0,   texture = 0},  -- Vest
+    ["decals"]      = { item = 0,  texture = 0},  -- Decals
+    ["accessory"]   = { item = 0, texture = 0},  -- Neck
+    ["bag"]         = { item = 0,   texture = 0},  -- Bag
+    ["pants"]       = { item = 43,  texture = 0},  -- Pants
+    ["shoes"]       = { item = -1,  texture = 0},  -- Shoes
+    ["mask"]        = { item = 0, texture = 0},  -- Mask
+    ["hat"]         = { item = 60, texture = 0},  -- Hat
+    ["glass"]       = { item = 0,   texture = 0},  -- Glasses
+    ["ear"]         = { item = 0,   texture = 0},  -- Ear accessories
+}
