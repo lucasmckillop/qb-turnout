e78e1e75e806da34d175487adbf310989652bbb0
peachy#9999 <90997593+lucasmckillop@users.noreply.github.com>
2022-04-12 22:45:03 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'qb-turnoutgear'
+version '2.0.0'
+author 'peachy#9999'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'cl_clothing.lua'
+}
+
+lua54 'yes'
